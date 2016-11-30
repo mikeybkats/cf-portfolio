e3b83100213da92e995e42dffdde80bab8207943
mikeybkats <michaelbkats@gmail.com>
2016-11-29 21:44:17 -0800

    adding javascript files and post content... giddy with excitement.

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+var posts = [];
+
+function Post (options){
+  this.title = options.title;
+  this.datePublished = options.datePublished;
+  this.author = options.author;
+  this.body = options.body;
+}
---
+++ scripts/posts.js
@@ -0,0 +1,22 @@
+var blogPosts = [
+  {
+    title: 'how to make a pair of shoes',
+    datePublished: '2016-11-26',
+    author: 'michael barakat',
+    body: '<img src="" /><p>some text about making shoes.</p>'
+  },
+
+  {
+    title: 'how to make a fashion accessories brand',
+    datePublished: '2016-11-28',
+    author: 'michael barakat',
+    body: '<img src="" /><p>some text about building a fashion brand.</p>'
+  },
+
+  {
+    title: 'how to be a photographer',
+    datePublished: '2016-11-28',
+    author: 'michael barakat',
+    body: '<img src="" /><p>some text about being a photographer.</p>'
+  }
+];
