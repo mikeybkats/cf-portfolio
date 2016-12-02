fcdcb95b0174fc980a54e41e7b91274fef500b4c
mikeybkats <michaelbkats@gmail.com>
2016-12-02 09:26:39 -0800

    making Handlebars template

--- scripts/postsAggregate.js
+++ scripts/postsAggregate.js
@@ -8,17 +8,29 @@ function Post (options){
   this.body = options.body;
 }
 
-// make a prototype post that passes new posts into the HTML template.
+// class04 make a new function prototype that works with the handlebars template in the index.html
 Post.prototype.toHtml = function(){
-  var $newPost = $('article.template').clone();
-  $newPost.find('h1').text(this.title);
-  $newPost.find('time').text(this.datePublished);
-  $newPost.find('.author').text(this.author);
-  $newPost.find('.post-content').append(this.body);
-  // remove the template class to show the object
-  $newPost.removeClass('template');
-  return $newPost;
+  // add any functions that implement the prototype object
+
+  // put in the handlebars template script and assign it to a new var
+  var $templateScript = $('#postsTemplate');
+  // compile the template and assign it to a new var
+  var compiledTemplate = handlebars.compile($templateScript);
+  // return the compiled template
+  return compiledTemplate(this);
 };
+
+// class03 make a prototype post that passes new posts into the HTML template.
+// Post.prototype.toHtml = function(){
+//   var $newPost = $('article.template').clone();
+//   $newPost.find('h1').text(this.title);
+//   $newPost.find('time').text(this.datePublished);
+//   $newPost.find('.author').text(this.author);
+//   $newPost.find('.post-content').append(this.body);
+//   // remove the template class to show the object
+//   $newPost.removeClass('template');
+//   return $newPost;
+// };
 
 // sort the blog posts by newest to oldest
 blogPosts.sort(function(currentObject, nextObject){
