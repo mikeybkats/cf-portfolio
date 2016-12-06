bcfddc6c358b71a9a498181e6e1edb7970258b68
mikeybkats <michaelbkats@gmail.com>
2016-12-06 00:06:04 -0800

    not working yet

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-var sectionViewer = {};
+var postsViewer = {};
 
 // menu close after li click
 $(document).ready(function(){
@@ -8,15 +8,23 @@ $(document).ready(function(){
 });
 
 // menu functionality: on click hide other sections
-sectionViewer.navigationFilter = function(){
+postsViewer.navigationFilter = function(){
   $('.main-nav').on('click', '.nav-item',function(){
     // Hide all of the sections that contain unwanted content
     $('.page-content').hide();
     // Fade in the single section that is wanted
     $('#' + $(this).data('spec')).fadeIn('slow');
+    // sort of makeshift way to scroll back to the top
     window.setTimeout(function(){window.scrollTo(0, 0);}, 100);
   });
   $('.main-nav .menu-item:first').click();
 };
 
-sectionViewer.navigationFilter();
+postsViewer.renderToPage = function(){
+  Post.allPosts.forEach(function(blogPostObj) {
+    $('#posts').append(blogPostObj.toHtml());
+  });
+  postsViewer.navigationFilter();
+};
+
+Post.fetchAll();
--- scripts/postsAggregate.js
+++ scripts/postsAggregate.js
@@ -1,35 +1,47 @@
-var posts = [];
-
-// make a constructor to build the blog posts based on the HTML template.
-function Post (options){
-  this.title = options.title;
-  this.datePublished = options.datePublished;
-  this.author = options.author;
-  this.body = options.body;
+function Post (opts) {
+  for (var keys in opts) {
+    this[keys] = opts[keys];
+  }
 }
 
-// class04 make a new function prototype that works with the handlebars template in the index.html
+Post.allPosts = [];
+
 Post.prototype.toHtml = function(){
-  // put in the handlebars template script and assign it to a new var
   var $templateScript = $('#postsTemplate').html();
-  // compile the template and assign it to a new var
   var compiledTemplate = Handlebars.compile($templateScript);
-  // return the compiled template
   return compiledTemplate(this);
 };
 
-// sort the blog posts by newest to oldest
-blogPosts.sort(function(currentObject, nextObject){
-  return (new Date(nextObject.datePublished)) - (new Date(currentObject.datePublished));
-});
+Post.loadAll = function(inputData) {
+  inputData.sort(function(a,b) {
+    return (new Date(b.datePublished)) - (new Date(a.datePublished));
+  })
+  .forEach(function(ele) {
+    Post.allPosts.push(new Post(ele));
+  });
+};
 
+Post.fetchAll = function() {
+  // if (localStorage){
+  //
+  // }else{
+    $.getJSON('data/posts.json', function(postsJSON){
+      Post.loadAll(postsJSON);
+      localStorage.setItem('posts', JSON.stringify(postsJSON));
+      
+    });
+    postsViewer.renderToPage();
+  // }
+};
+// // sort the blog posts by newest to oldest
+// blogPosts.sort(function(currentObject, nextObject){
+//   return (new Date(nextObject.datePublished)) - (new Date(currentObject.datePublished));
+// });
+//
 // push the new posts into the array
-blogPosts.forEach(function(blogPostObj){
-  posts.push(new Post(blogPostObj));
-});
-
-// append posts to DOM
-// cycles through all the posts (which have just been pushed to the posts[], and for each it passes them through to line 36
-posts.forEach(function(blogPostObj) {
-  $('#posts').append(blogPostObj.toHtml());
-});
+// blogPosts.forEach(function(blogPostObj){
+//   posts.push(new Post(blogPostObj));
+// });
+//
+// // append posts to DOM
+// // cycles through all the posts (which have just been pushed to the posts[], and for each it passes them through to line 34
