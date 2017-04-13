426128a329afcf63aeeb222a298f3be848fcd49c
root <root@tech-portfolio.localdomain>
2017-04-13 20:27:44 +0000

    server adjustment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
   requestProxy = require('express-request-proxy'),
   // NOTE: require in our request proxy module
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 3001,
   app = express();
 
 app.use(express.static('./'));
