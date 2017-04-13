a4fe93881cfef3929715d12028eb5da3bd2011e5
mikeybkats <michaelbkats@gmail.com>
2017-04-13 14:07:25 -0700

    fixing navigation interactivity

--- scripts/controllers/menu-scroll-lock.js
+++ scripts/controllers/menu-scroll-lock.js
@@ -1,23 +1,21 @@
 $(document).ready(function(){
   var open = false;
+
   $('.nav-item a').on('click', function(e){
     open = false;
+    unbindScroll();
   });
+
   $('.icon-menu').not('.nav-item a').on('click',function(e){
     $('.nav-item a').css('display', 'block');
 
     if(open === false){
+      bindScroll();
+
       $('html, body').on('touchmove', function(e){e.preventDefault();});
+
       $('.nav-item a').css('display', 'block');
-      var scrollPosition = [
-        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-        self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-      ];
-      var html = jQuery('html');
-      html.data('scroll-position', scrollPosition);
-      html.data('previous-overflow', html.css('overflow'));
-      html.css('overflow', 'hidden');
-      window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
       open = true;
       console.log(open);
       return;
@@ -26,11 +24,8 @@ $(document).ready(function(){
     if(open === true){
       $('html, body').unbind('touchmove').unbind();
       $('.nav-item a').css('display', 'none');
+      unbindScroll();
 
-      var html = $('html');
-      var scrollPosition = html.data('scroll-position');
-      html.css('overflow', html.data('previous-overflow'));
-      window.scrollTo(scrollPosition[0], scrollPosition[1]);
       open = false;
       console.log(open);
       return;
@@ -45,3 +40,22 @@ $(document).ready(function(){
   }
 
 });
+
+function bindScroll(){
+  var scrollPosition = [
+    self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+    self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+  ];
+  var html = jQuery('html');
+  html.data('scroll-position', scrollPosition);
+  html.data('previous-overflow', html.css('overflow'));
+  html.css('overflow', 'hidden');
+  window.scrollTo(scrollPosition[0], scrollPosition[1]);
+}
+
+function unbindScroll(){
+  var html = $('html');
+  var scrollPosition = html.data('scroll-position');
+  html.css('overflow', html.data('previous-overflow'));
+  window.scrollTo(scrollPosition[0], scrollPosition[1]);
+}
