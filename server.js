3bc21cb40098ce62ddb62235de51daac32854cc0
mikeybkats <michaelbkats@gmail.com>
2017-04-13 11:33:29 -0700

    fixing navigation interactivity

--- server.js
+++ server.js
@@ -4,18 +4,6 @@ var express = require('express'),
   port = process.env.PORT || 3000,
   app = express();
 
-// NOTE: now use our proxy within a function to request
-//        our github data on the server.
-//var proxyGithub = function(request, response) {
-//  console.log('Routing GitHub request for', request.params[0]);
-//  (requestProxy({
-//    url: 'https://api.github.com/' + request.params[0],
-//    headers: {Authorization: 'token ' + process.env.GITHUB_TOKEN}
-//  }))(request, response);
-//};
-
-// app.get('/github/*', proxyGithub);
-
 app.use(express.static('./'));
 
 app.get('*', function(request, response) {
