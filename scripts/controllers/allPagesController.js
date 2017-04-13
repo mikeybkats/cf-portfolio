29bf3f3e8551f7cc475631556f8613157041ce24
mikeybkats <michaelbkats@gmail.com>
2017-04-12 17:37:41 -0700

    styling readjust

--- scripts/controllers/allPagesController.js
+++ scripts/controllers/allPagesController.js
@@ -3,9 +3,13 @@
 
   allPagesController.reveal = function (){
     $('.second-nav').hide();
+    $('section.title-section').hide();
     $('section.title-section').siblings().hide();
+    $('section.title-section').fadeIn('slow');
     $('section.title-section').siblings().fadeIn('slow');
   };
+
+
 
   module.allPagesController = allPagesController;
 
